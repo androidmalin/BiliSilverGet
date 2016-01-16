30382bd63edaef80220422e000c9d13fa027753a
qhgz2013 <qhgz2011@hotmail.com>
2016-01-16 20:30:57 +0800

    CookieDebugger v1
    好吧应该不会出错了

--- VBUtil/CookieDebugger.vb
+++ VBUtil/CookieDebugger.vb
@@ -9,11 +9,13 @@ Public Class CookieDebugger
     End Sub
     Private Sub CookieDebugger_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
+        ProgressBar1.Maximum = _RESET_TIME
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim ts As New TimeSpan(0, 0, _time)
         lblTime.Text = ts.ToString
+        ProgressBar1.Value = _RESET_TIME - _time
         If _time = 0 Then Refresh_Item()
 
         _time -= 1
@@ -21,9 +23,8 @@ Public Class CookieDebugger
 
     Private Sub Refresh_Item()
         _time = _RESET_TIME
-        Dim lstCookie As New List(Of Cookie)
+        ListView1.Items.Clear()
         Dim table As Hashtable = Utils.NetUtils.DefaultCookieContainer.GetType().InvokeMember("m_domainTable", Reflection.BindingFlags.NonPublic Or Reflection.BindingFlags.GetField Or Reflection.BindingFlags.Instance, Nothing, Utils.NetUtils.DefaultCookieContainer, New Object() {})
-
 
         For Each pathList As Object In table.Values
             Dim lstCookieCol As SortedList = pathList.GetType().InvokeMember("m_list", Reflection.BindingFlags.NonPublic Or Reflection.BindingFlags.GetField Or Reflection.BindingFlags.Instance, Nothing, pathList, New Object() {})
